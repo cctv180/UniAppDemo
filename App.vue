5ba9398270bb54ab1c4db8722210b2b5d45d5d7c
HPw19e <cctv180@gmail.com>
2020-10-29 14:50:07 +0800

    git 启用托管

---
+++ App.vue
@@ -0,0 +1,17 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch 应用初始化完成')
+		},
+		onShow: function() {
+			console.log('App Show 应用启动，或从后台进入前台显示')
+		},
+		onHide: function() {
+			console.log('App Hide 应用从前台进入后台')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
---
+++ README.md
@@ -0,0 +1,1 @@
+# UniAppDemo
