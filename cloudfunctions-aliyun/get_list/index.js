99c146b8580ed159fa595ae67e75eeb1abae5748
HPw19e <cctv180@gmail.com>
2020-11-02 17:28:10 +0800

    P23 4-5云数据库的添加和删除

--- cloudfunctions-aliyun/get_list/index.js
+++ cloudfunctions-aliyun/get_list/index.js
@@ -1,25 +1,58 @@
 'use strict';
+const db = uniCloud.database()
 exports.main = async (event, context) => {
-	//event为客户端上传的参数
+    //event为客户端上传的参数
     //context 包含调用信息和运行状态，获取每次调用的上下文
-	console.log('event : ', event)
+
+    const collection = db.collection('user')
+
+    // add 增加一条数据
+    // let res = await collection.add(
+    //     [{
+    //         name: 'uni-App'
+    //     },
+    //     {
+    //         name: 'html',
+    //         type: '前端'
+    //     }]
+    // )
+
+    // remove 删除一条数据
+    //const res = await collection.doc('5f9f9e495f32d30001fe7ca3').remove()
+
+    // update 更新一条数据
+    // const res = await collection.doc('5f9f9d84ab6ef400017f979c').update({
+    //     type:'html'
+    // })
     
-    //context中可获取客户端调用的上下文
-      let os = context.OS //客户端操作系统，返回值：android、ios    等
-      let platform = context.PLATFORM //运行平台，返回值为 mp-weixin、app-plus等
-      let appid = context.APPID // manifest.json中配置的appid
-      let clientIP = context.CLIENTIP // 客户端ip信息
-      let clientUA = context.CLIENTUA // 客户端user-agent
+    // set 替换更新一条记录 
+    // const res = await collection.doc('5f9f9d84ab6ef400017f979c').set({
+    //     type:'html set'
+    // })    
     
-	//返回数据给客户端
-	return {
-        code:200,
-        msg:event.name + '的年龄是' + event.age,
-        os,
-        platform,
-        appid,
-        clientIP,
-        clientUA,
+    // set 替换更新一条记录 
+    // const res = await collection.doc('aabbccdd').set({
+    //     type:'html set',
+    //     name:'aabb'
+    // })
+
+    // get 获取一条记录 
+    //const res = await collection.doc('aabbccdd').get()
+    
+    
+    // get 获取一条记录 
+    const res = await collection.where({
+        name:'LiMing'
+    })
+
+    console.log("数据操作:")
+    console.log(JSON.stringify(res))
+
+    //返回数据给客户端
+    return {
+        code: 200,
+        msg:'查询成功',
+        data:res
         //context   //context不能直接返回
     }
 };
