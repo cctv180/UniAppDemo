459182786fd32562e9dcffb5246d2fe785101efc
HPw19e <cctv180@gmail.com>
2020-11-06 11:19:46 +0800

    P27 5-2自定义导航栏

--- pages/tabbar/index/index.vue
+++ pages/tabbar/index/index.vue
@@ -1,52 +1,34 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+    <view class="content">
+        <!-- 自定义组件 navbar导航栏 -->
+        <navbar></navbar>
+        <view v-for="item in 100">
+        {{item}}测试行
+        </view>
+    </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
+    // easyCom components/组件名/组件名.vue局部引入 同名免引入
+    // import navbar from '@/components/navbar/navbar.vue'
+    export default {
+        // components:{
+        //     navbar
+        // },
+        data() {
+            return {
+                title: 'Hello'
+            }
+        },
+        onLoad() {
 
-		},
-		methods: {
+        },
+        methods: {
 
-		}
-	}
+        }
+    }
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+<style lang="scss">
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
 </style>
